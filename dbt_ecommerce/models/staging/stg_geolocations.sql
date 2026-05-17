74d3ee1d7f024bd8380485f896cba4e0f794b3e9
Lê Tuấn Minh Thành <letuanminhthanh@Lee-Tun-here.local>
2026-05-17 23:32:50 +0700

    adjust, clean the table to be unique zipcode

--- dbt_ecommerce/models/staging/stg_geolocations.sql
+++ dbt_ecommerce/models/staging/stg_geolocations.sql
@@ -1,7 +1,21 @@
-select
-    cast(geolocation_zip_code_prefix as integer) as geolocation_zip_code_prefix,
-    cast(geolocation_lat as float) as geolocation_lat,
-    cast(geolocation_lng as float) as geolocation_lng,
-    geolocation_city,
-    geolocation_state 
-from {{ source('olist_raw', 'geolocations') }}
+with source as (
+    select * from {{ source('olist_raw', 'geolocation') }}
+),
+
+grouped as (
+    select
+        geolocation_zip_code_prefix as zip_code_prefix,
+        -- take the average lat and lng to figure out the middle point of zipcode's area
+        avg(geolocation_lat) as lat,
+        avg(geolocation_lng) as lng,
+        
+        -- because 1 zip code can be write by many city's names like": Sao Paulo, São Paulo , 
+        -- we take one name as representative. in this case i used Max()
+        max(trim(lower(geolocation_city))) as city,
+        max(upper(geolocation_state)) as state
+        
+    from source
+    group by 1
+)
+
+select * from grouped
