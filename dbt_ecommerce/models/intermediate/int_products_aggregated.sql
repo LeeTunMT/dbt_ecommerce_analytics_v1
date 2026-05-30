d2951a7ba310d5292f7928cbb5f899c1e236f202
Tên GitHub của bạn <email_gn_vi_github@example.com>
2026-05-30 09:07:36 +0700

    add exta feature engineering

--- dbt_ecommerce/models/intermediate/int_products_aggregated.sql
+++ dbt_ecommerce/models/intermediate/int_products_aggregated.sql
@@ -9,8 +9,19 @@ with source as (
 agg_products as (
     select
         product_id,
-        sum(price) as total_goods_revenue
-        sum(freight_value) as total_freight_revenue
+
+        count(distinct order_id) as total_orders,
+        count(*) as total_order_items,
+        sum(price) as total_product_revenue,
+        sum(freight_value) as total_freight_value,
+        sum(price + freight_value) as total_gmv,
+        avg(price) as avg_item_price,
+        avg(freight_value) as avg_freight_value,
+        max(price) as max_price,
+        min(price) as min_price,
+        min(shipping_limit_at) as first_shipping_limit_at,
+        max(shipping_limit_at) as last_shipping_limit_at
+
     from source
     group by product_id
 ),
